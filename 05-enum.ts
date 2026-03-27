a693b64646e46eae0c9455129cf11cae9d6ef444
auhsojp <jashuapatenia15@gmail.com>
2026-03-27 16:24:05 +0800

    Done playing and updating the 05-enum.ts file

--- 05-enum.ts
+++ 05-enum.ts
@@ -1,14 +1,17 @@
 (() => {
     enum Role {
         // SuperAdmin,
-        // SuperAdmin = 1,
-        SuperAdmin = 'SuperAdmin',
-        // Teacher,
-        Teacher = 'Teacher',
-        // Student,
-        Student = 'Student',
+        SuperAdmin = 1,
+        // Admin,
+        Admin = 'Admin',
+        // Officer,
+        Officer = 'Officer',
+        // Member,
+        Member = 'Member',
     }
 
-    // let userRole: Role = 0;
-    let userRole: Role = Role.SuperAdmin;
+    let userRole: Role = 1;
+    // let userRole: Role = Role.SuperAdmin;
+
+    console.log(userRole)
 })
