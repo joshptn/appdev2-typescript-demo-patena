46cf35394240b271f0f33f196b563f8369ed17b7
auhsojp <jashuapatenia15@gmail.com>
2026-04-13 20:26:07 +0800

    Done playing and updating the 09-special-types.ts

--- 09-special-types.ts
+++ 09-special-types.ts
@@ -2,24 +2,40 @@
 let a: null;
 
 a = null
-// a = "Hi"
+
+// a = 0 error type null
+// a = "Hi" error type null
 
 let b: null | string
 
 b = null
 b = "Hi"
-// b = 10
+
+// b = true error, a boolean
+// b = 10 error, a number
 
 // Special Type - undefined
 let c: undefined;
 
 c = undefined
-// c = null
-// c = "Hi"
+// c = null error, a null
+// c = "Hi" error, a string
+// c = true error, a boolean
+// c = 5 error, a number
 
-let d: undefined | string
+let d: undefined | string 
 
 d = undefined
-d = "Hi"
-// d = null
-// d = 10
+d = "Hi" 
+
+// d = true error, a bolean
+// d = null error, a null
+// d = 10 error, number 
+
+let e: undefined | string | boolean | number | null
+
+e = undefined
+e = "Hi" 
+e = true 
+e = null 
+e = 10 
